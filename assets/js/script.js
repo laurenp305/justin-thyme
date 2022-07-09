fc477879b7ecc38e11334bdfbb0fef500c2b1886
laurenp305 <lamor1800@gmail.com>
2022-07-09 10:41:05 -0400

    added js file with acceptance criteria

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// WHEN I open the planner the current day is displayed at the top of the calendar 
+// WHEN I scroll down I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block I can enter an event
+// WHEN I click the save button for that time block the text for that event is saved in local storage
+// WHEN I refresh the page the saved events remain
