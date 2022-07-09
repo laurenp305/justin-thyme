93b484012def9d164261c263da252f0feeff04a7
laurenp305 <lamor1800@gmail.com>
2022-07-09 12:57:54 -0400

    committing again to make sure work is saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // WHEN I open the planner the current day is displayed at the top of the calendar 
 $(document).ready(function () {
-    let NowMoment = moment().format("MMMM Do YYYY");
+    let NowMoment = moment().format('MMMM Do YYYY');
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
