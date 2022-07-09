e656aacf8bd6be41afc0e12ea99db15b12ae7ac4
laurenp305 <lamor1800@gmail.com>
2022-07-09 12:46:58 -0400

    added functions for past present future time

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,20 @@ $(document).ready(function () {
     $("textarea").val("");
     localStorage.clear();
   });
+
+//Grabs hours from every time slot to compare to real time (past, present, future)
+   $(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+
+    if (currentHour == timeDiv) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+
