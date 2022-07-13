f6df9bcc97f0b4848e701a2b1182c0f43731703c
laurenp305 <lamor1800@gmail.com>
2022-07-13 17:11:54 -0400

    added clear fields button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
+
+// Button function to clear local storage and contents
+$("#clearFieldsBtn").click(function (event) {
+  event.preventDefault;
+  $("textarea").val("");
+  localStorage.clear();
+});
