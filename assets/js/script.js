8de24e954ef43389196ceed410f34adcece2d641
laurenp305 <lamor1800@gmail.com>
2022-07-13 17:25:58 -0400

    added id for currentTime & function for a continous timer at top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,55 @@
 // WHEN I open the planner the current day is displayed at the top of the calendar 
 $(document).ready(function () {
-    let NowMoment = moment().format('MMMM Do YYYY');
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = NowMoment;
-    let currentHour = moment().format("HH");
+  let NowMoment = moment().format("MMMM Do YYYY");
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = NowMoment;
+  let currentHour = moment().format("HH");
 
-// Button function to clear local storage and contents
-$("#clearFieldsBtn").click(function (event) {
-  event.preventDefault;
-  $("textarea").val("");
-  localStorage.clear();
-});
-
-  //grabs hour from each time slot and compares it to real time
-  $(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentHour == timeDiv) {
-      $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
-    } else if (currentHour < timeDiv) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
-    }
+//adds timer on top of page that gives real time
+function timer() {
+  var time = moment().format('h:mm:ss a');
+  var currentTime = document.querySelector("#currentTime");
+  currentTime.textContent = time;
+}
+setInterval(timer, 1000)
+// Clear fields function
+  $("#clearFieldsBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
   });
 
-   //grabs values from time and value divs and saves them to local storage
-   $(".saveBtn").click(function (event) {
+// WHEN I view the time blocks for that day each time block is color-coded to indicate whether it is in the past, present, or future
+$(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+    if (currentHour == timeDiv) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+
+// WHEN I refresh the page the saved events remain
+$(".saveBtn").click(function (event) {
     event.preventDefault();
     var value = $(this).siblings(".time-block").val();
     var time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, value);
   });
   
-    //retrieves items from local storage and sets them in proper places
-    $("#hour-09 .time-block").val(localStorage.getItem("09"));
-    $("#hour-10 .time-block").val(localStorage.getItem("10"));
-    $("#hour-11 .time-block").val(localStorage.getItem("11"));
-    $("#hour-12 .time-block").val(localStorage.getItem("12"));
-    $("#hour-13 .time-block").val(localStorage.getItem("13"));
-    $("#hour-14 .time-block").val(localStorage.getItem("14"));
-    $("#hour-15 .time-block").val(localStorage.getItem("15"));
-    $("#hour-16 .time-block").val(localStorage.getItem("16"));
-    $("#hour-17 .time-block").val(localStorage.getItem("17"));
-  });
+// Takes items from local storage and places them in appropriate time blocks
+  $("#hour-09 .time-block").val(localStorage.getItem("09"));
+  $("#hour-10 .time-block").val(localStorage.getItem("10"));
+  $("#hour-11 .time-block").val(localStorage.getItem("11"));
+  $("#hour-12 .time-block").val(localStorage.getItem("12"));
+  $("#hour-13 .time-block").val(localStorage.getItem("13"));
+  $("#hour-14 .time-block").val(localStorage.getItem("14"));
+  $("#hour-15 .time-block").val(localStorage.getItem("15"));
+  $("#hour-16 .time-block").val(localStorage.getItem("16"));
+  $("#hour-17 .time-block").val(localStorage.getItem("17"));
+});
