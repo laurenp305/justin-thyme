d25fa1f87f2cfa3b9e64359e8a5d03d832f387fa
laurenp305 <lamor1800@gmail.com>
2022-07-13 17:13:07 -0400

    added function to highlight text blocks according to real time

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,19 @@ $("#clearFieldsBtn").click(function (event) {
   $("textarea").val("");
   localStorage.clear();
 });
+
+  //grabs hour from each time slot and compares it to real time
+  $(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+    
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
